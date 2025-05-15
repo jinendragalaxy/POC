52f2aa7e44d04a766506818f90bd6284174a4e9b
jsdodiya <jinendradodiya9@gmail.com>
2025-05-15 10:33:30 +0530

    comments added

--- WEEK 2/14-May-2025/Vue/vue-project-4/src/components/LifecycleDemo.vue
+++ WEEK 2/14-May-2025/Vue/vue-project-4/src/components/LifecycleDemo.vue
@@ -2,14 +2,17 @@
     <div class="lifecycle-demo">
       <h2>Lifecycle Hooks Demo</h2>
       <p>Check console for lifecycle hook logs</p>
+      <!-- IF PRODUCTS ARE AVAIBLE THAN IT SHOWS THEM -->
       <div v-if="products.length">
         <h3>Products from API:</h3>
         <ul>
+          <!-- LISTING OF ITEMS -->
           <li v-for="product in products" :key="product.id">
             {{ product.title }} - ${{ product.price }}
           </li>
         </ul>
       </div>
+      <!-- OTHERWISE ELSE CONDTION -->
       <div v-else>
         <p>Loading products...</p>
       </div>
@@ -21,35 +24,45 @@
     name: 'LifecycleDemo',
     data() {
       return {
-        products: []
+        // PRODUCT LIST STORE HERE
+        products: []  
       }
     },
+    // CALLED BEFORE CREATING THE COMPONENT
     beforeCreate() {
       console.log('beforeCreate: Component initialization begins')
     },
+    // CALLED AFTER CREATING THE COMPONENT
     created() {
       console.log('created: Component is created, data is reactive')
       this.fetchProducts()
     },
+    // CALEED BEFORE MOUNTING IN THE DOM
     beforeMount() {
       console.log('beforeMount: Template compilation completed, before DOM insertion')
     },
+    // CALLED AFTER MOUNTING IN THE DOM
     mounted() {
       console.log('mounted: Component is mounted to DOM')
     },
+    // CALLED BEFORE UPDATING DOM
     beforeUpdate() {
       console.log('beforeUpdate: Before DOM re-render')
     },
+    // CALLED AFTER UPDATING DOM
     updated() {
       console.log('updated: After DOM re-render')
     },
+    // CALLED BEFORE DESTROYING COMPONENT
     beforeDestroy() {
       console.log('beforeDestroy: Before component destruction')
     },
+    // CALLED AFTER THE COMPONENT IS DESTROYED
     destroyed() {
       console.log('destroyed: Component is destroyed')
     },
     methods: {
+      // FETCHING PRODUCTS FROM ASYNC AWAIT
       async fetchProducts() {
         try {
           const response = await fetch('https://dummyjson.com/products')
--- WEEK 2/14-May-2025/Vue/vue-project-4/src/components/UserDetail.vue
+++ WEEK 2/14-May-2025/Vue/vue-project-4/src/components/UserDetail.vue
@@ -2,11 +2,13 @@
     <div class="user-detail">
       <h2>User Details</h2>
       <p>Showing details for user ID: {{ id }}</p>
+      <!-- IF USER DATA IS AVAILABLE THAN DISPLAY IT -->
       <div v-if="user">
         <h3>{{ user.username }}</h3>
         <p>Email: {{ user.email }}</p>
         <p>Phone: {{ user.phone }}</p>
       </div>
+      <!-- ELSE CONDTION OTHERWISE -->
       <div v-else>
         <p>Loading user data...</p>
       </div>
@@ -16,17 +18,20 @@
   <script>
   export default {
     name: 'UserDetail',
+    // PROPS RECEIVED FROM THE PARENT COMPONENT
     props: {
       id: {
         type: String,
         required: true
       }
     },
+    // TO STORE INITIAL REACTIVE DATA 
     data() {
       return {
         user: null
       }
     },
+    // CALLED AFTER THE COMPONENT IS CREATED AND FETCH USER DATA
     created() {
       this.fetchUser()
     },
@@ -40,6 +45,7 @@
         }
       }
     },
+    // WE USE WATCHER TO LISTEN FOR THE CHANGES IN THE ID PROP
     watch: {
       id() {
         this.fetchUser()

3eb218622151db36b93e0121588baa670062f384
jsdodiya <jinendradodiya9@gmail.com>
2025-05-15 17:20:33 +0530

    child-to-parent

---
+++ WEEK 2/15-May-2025/child-to-parents/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <br>
+    <!-- Listen for 'change-title' event -->
+    <Child @change-title="updateTitle" />
+  </div>
+</template>
+
+<script>
+import Child from './components/Child.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Child
+  },
+  data() {
+    return {
+      title: "Parents title First"
+    };
+  },
+  methods: {
+    updateTitle(newTitle) {
+      this.title = newTitle;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ WEEK 2/15-May-2025/child-to-parents/src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <button @click="updateTitle">Update parent title</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Child',
+    methods: {
+      updateTitle() {
+        this.$emit('change-title', "Parents Title Changed");
+      }
+    }
+  };
+  </script>
+  

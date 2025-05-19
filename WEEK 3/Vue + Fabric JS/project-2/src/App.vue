e419d88fef0cabad8ead8c55052053fe18100ff6
jsdodiya <jinendradodiya9@gmail.com>
2025-05-19 11:52:31 +0530

    Project -2 (fabric js)

---
+++ WEEK 3/Vue + Fabric JS/project-2/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app" style="padding:16px; font-family:Arial">
+    <h2>Shape Drawer Lite</h2>
+    <FabricLite/>
+  </div>
+</template>
+
+<script>
+import FabricLite from '../src/components/FabricLite.vue'
+export default { components: { FabricLite } }
+</script>
+
+
